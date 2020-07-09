a92465596fe718b1ca169e24fd50d90dfeac49aa
rodmansini <rodmfabricio@gmail.com>
2020-07-08 23:14:28 -0600

    cambio1

--- tarea.cpp
+++ tarea.cpp
@@ -1,2 +1,40 @@
 #include<iostream>
-using namespace
+using namespace std;
+int main()
+
+{
+    int tipodecliente;
+    double cantidad, precio, subtotal=0, descuento=0, totalpagar=0;
+    cout<<" ingrese el tipo de cliene  ";
+    cin >> tipodecliente;
+    cout<<"ingrese la cantidad:   ";
+    cin>> cantidad;
+    cout<< "ingrese el precio   ";
+    cin >>precio;
+    subtotal=cantidad*precio;
+    //si el subtotal=>1000 y tipo de cliente==1 entonces el descuento = subtotal*0.1
+    //si el subtotal=>10000 y tipo de cliente==1 entonces el descuento = subtotal*0.15
+    if ((subtotal>1000)&&(tipodecliente==1))
+    {
+        descuento=subtotal*0.1;
+        totalpagar=subtotal-descuento;
+ 
+    }
+    else if ((subtotal>10000)&&(tipodecliente==2))
+    {
+        descuento=subtotal*0.15;
+        totalpagar=subtotal-descuento;
+        
+    }
+    else
+    {
+        descuento=subtotal*0;
+        totalpagar=subtotal-descuento;
+    
+    }
+      cout <<"subtotal es de  "<< subtotal << endl;
+        cout<<"su descuento es de  "<< descuento << endl;
+        cout <<" su total a pagar es de  "<< totalpagar;
+    
+    return 0;
+}
